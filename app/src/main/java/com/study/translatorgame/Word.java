e11736e2319091ae206fb388a52705eb3e761757
OlgaZ <olga.z.kordik@gmail.com>
2019-07-31 10:27:11 +0300

    Создан класс Word

---
+++ app/src/main/java/com/study/translatorgame/Word.java
@@ -0,0 +1,26 @@
+package com.study.translatorgame;
+
+import java.util.ArrayList;
+
+public class Word {
+    private String name;
+    private ArrayList<String> translation;
+
+    public Word(String name, ArrayList<String> translation) {
+        this.name = name;
+        this.translation = translation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getTranslation() {
+        return translation;
+    }
+
+    public boolean isHasTranslation(String s) {
+        if (translation == null) return false;
+        else return (translation.contains(s));
+    }
+}
